581e83c070875cd72ea5c395437daeae95fabb7d
«VIKTOR <mrgyugo@yandex.ru>
2017-10-24 13:49:34 +0300

    add struct Stack

---
+++ DataStructureAndAlgorithms/DataStructureAndAlgorithms/AppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  DataStructureAndAlgorithms
+//
+//  Created by Mac_Work on 24.10.17.
+//  Copyright © 2017 Mac_Work. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            window.backgroundColor = UIColor.white
+            window.rootViewController = UIViewController()
+            window.makeKeyAndVisible()
+        }
+        return true
+    }
+}
+
---
+++ DataStructureAndAlgorithms/DataStructureAndAlgorithms/Stack.swift
@@ -0,0 +1,108 @@
+//
+//  Stack.swift
+//  DataStructureAndAlgorithms
+//
+//  Created by Mac_Work on 24.10.17.
+//  Copyright © 2017 Mac_Work. All rights reserved.
+//
+
+import Foundation
+
+
+//STACK-------------------------
+
+/*
+ Стек реализует следующие три метода:
+ 
+ • push() - Добавляет элемент в конец стека
+ • pop() - Удаляет и возвращает элемент из вершины стека
+ • peek() - Возвращает верхний элемент из стека, но не удаляет его
+ 
+ Общие реализации могут также включать вспомогательные операции, такие как:
+ 
+ • count - Возвращает количество элементов в стеке
+ • isEmpty() - Возвращает true, если стек пуст, и false в противном случае
+ • isFull() - Если стек ограничивает количество элементов, этот метод возвращает true, если он заполнен и false в противном случае
+ */
+
+
+//Implementation
+
+
+public struct Stack<Element> {
+    
+    fileprivate var elements = [Element]()
+    public init() {}
+    
+    public mutating func pop () -> Element? {
+        return self.elements.popLast()
+    }
+    
+    public mutating func push (_ element: Element) {
+        self.elements.append(element)
+    }
+    
+    public mutating func peek () -> Element? {
+        return self.elements.last
+    }
+    
+    public var isEmpty: Bool {
+        return self.elements.isEmpty
+    }
+    
+    public var count: Int {
+        return self.elements.count
+    }
+    
+    public init<S : Sequence>(_ s: S) where S.Iterator.Element == Element {
+        self.elements = Array(s.reversed())
+    }
+}
+
+
+public struct ArrayIterator<Element> : IteratorProtocol {
+    
+    var currentElement: [Element]
+    init(elements: [Element]) {
+        self.currentElement = elements
+    }
+    
+    mutating public func next() -> Element? {
+        if (!self.currentElement.isEmpty) {
+            return self.currentElement.popLast()
+        }
+        return nil
+    }
+}
+
+
+
+//Protocols
+
+extension Stack: CustomStringConvertible, CustomDebugStringConvertible {
+    
+    public var description: String {
+        return self.elements.description
+    }
+    
+    public var debugDescription: String {
+        return self.elements.debugDescription
+    }
+    
+}
+
+
+extension Stack: ExpressibleByArrayLiteral {
+    public init(arrayLiteral elements: Element...) {
+        self.elements = elements
+    }
+}
+
+extension Stack: Sequence {
+    
+    public func makeIterator() -> ArrayIterator<Element> {
+        return ArrayIterator<Element>(elements: self.elements)
+    }
+    
+}
+
